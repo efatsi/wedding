fb1095248a25b3a4f8faacc1d7360f51a5081bb1
Eli Fatsi <eli.fatsi@viget.com>
2014-04-05 21:38:58 -0600

    css fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
-  def header_link_to(text, path)
-    if path == request.path
+  def header_link_to(text, paths)
+    paths = Array(paths)
+    if paths.include? request.path
       content_tag(:div, text, :class => "current")
     else
-      link_to text, path
+      link_to text, paths.first
     end
   end
 
